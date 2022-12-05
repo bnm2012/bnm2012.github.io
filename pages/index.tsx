33c1e6a138d60f163c72f062d3101b49cd32012a
bnm2012 <910733066@qq.com>
2022-12-06 01:42:24 +0800

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <div>
           <a href='/N2N/N2N_version.json'>
             <h2>
-            N2N_version.json11222
+            N2N_version.json
             </h2>
           </a>
         </div>
